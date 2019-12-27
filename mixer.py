49bdda40435a4f612b3e6b29b7baf89445e2a144
tsemkaloalena <alenatsemkalo@gmail.com>
2019-12-27 21:43:53 +0300

    Добавила класс с миксером. Серая кнопочка, когда он выключен. Если миксер включен, кнопочка зелёная. Миксер включается/выключается при нажатии.

--- mixer.py
+++ mixer.py
@@ -21,6 +21,7 @@ screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 all_sprites = pygame.sprite.Group()
 mixers = pygame.sprite.Group()
+button = pygame.sprite.Group()
 
 
 class Mixer(pygame.sprite.Sprite):
@@ -38,18 +39,16 @@ class Mixer(pygame.sprite.Sprite):
         for j in range(rows):
             for i in range(columns):
                 frame_location = (self.rect.w * i, self.rect.h * j)
-                self.frames.append(sheet.subsurface(pygame.Rect(
-                    frame_location, self.rect.size)))
+                self.frames.append(sheet.subsurface(pygame.Rect(frame_location, self.rect.size)))
+                self.mask = pygame.mask.from_surface(sheet)
 
     def update(self):
         self.cur_frame = (self.cur_frame + 1) % len(self.frames)
         self.image = self.frames[self.cur_frame]
 
-mixing = True
-if mixing:
-    mixer = Mixer(pygame.transform.scale(load_image("mixer_on.png", -1), (400, 200)), 2, 1, 100, 50)
-else:
-    mixer = Mixer(pygame.transform.scale(load_image("mixer_off.png", -1), (200, 200)), 1, 1, 100, 50)
+
+mixer = Mixer(pygame.transform.scale(load_image("mixer_off.png", -1), (200, 200)), 1, 1, 100, 50)
+mixing = False
 
 running = True
 while running:
@@ -57,6 +56,16 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if mixer.rect.collidepoint(event.pos):
+                if mixing:
+                    mixing = False
+                    mixers.remove(mixer)
+                    mixer = Mixer(pygame.transform.scale(load_image("mixer_off.png", -1), (200, 200)), 1, 1, 100, 50)
+                else:
+                    mixing = True
+                    mixers.remove(mixer)
+                    mixer = Mixer(pygame.transform.scale(load_image("mixer_on.png", -1), (400, 200)), 2, 1, 100, 50)
     mixers.update()
     screen.fill((0, 0, 0))
     mixers.draw(screen)

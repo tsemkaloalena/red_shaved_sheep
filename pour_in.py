85bab528ff8a7da31aead1e73dd6ee63cf9d4c44
tsemkaloalena <alenatsemkalo@gmail.com>
2019-12-29 14:38:52 +0300

    перчик

--- pour_in.py
+++ pour_in.py
@@ -20,14 +20,14 @@ size = width, height
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 all_sprites = pygame.sprite.Group()
-peppers = pygame.sprite.Group()
+pour_in_products = pygame.sprite.Group()
 button = pygame.sprite.Group()
-time = 50
+time = 30
 
 
-class Pepper(pygame.sprite.Sprite):
+class PourInProduct(pygame.sprite.Sprite):
     def __init__(self, sheet, columns, rows, x, y):
-        super().__init__(peppers)
+        super().__init__(pour_in_products)
         self.frames = []
         self.cut_sheet(sheet, columns, rows)
         self.cur_frame = 0
@@ -48,41 +48,30 @@ class Pepper(pygame.sprite.Sprite):
         self.image = self.frames[self.cur_frame]
 
 
-pepper = Pepper(pygame.transform.scale(load_image("pepper.png", -1), (66, 120)), 1, 1, 350, 350)
-# mixer = Mixer(pygame.transform.scale(load_image("pepper5.png", -1), (400, 241)), 2, 1, 100, 50)
-peppering = False
+pepper = PourInProduct(pygame.transform.scale(load_image("pepper.png", -1), (66, 120)), 1, 1, 350, 350)
+pouring_in = False
 
 running = True
 while running:
-    # screen.fill((0, 0, 0))
     screen.fill((0, 255, 100))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             if pepper.rect.collidepoint(event.pos):
-                if not peppering:
-                    peppers.remove(pepper)
-                    pepper = Pepper(pygame.transform.scale(load_image("pepper5.png", -1), (400, 241)), 2, 1, 100, 50)
-                    peppering = True
-
-        #         if mixing:
-        #             mixing = False
-        #             mixers.remove(mixer)
-        #             mixer = Mixer(pygame.transform.scale(load_image("mixer_off.png", -1), (200, 200)), 1, 1, 100, 50)
-        #         else:
-        #             mixing = True
-        #             mixers.remove(mixer)
-        #             mixer = Mixer(pygame.transform.scale(load_image("mixer_on.png", -1), (400, 200)), 2, 1, 100, 50)
-    peppers.update()
-    if peppering:
+                if not pouring_in:
+                    pour_in_products.remove(pepper)
+                    pepper = PourInProduct(pygame.transform.scale(load_image("pepper5.png", -1), (400, 241)), 2, 1, 100, 50)
+                    pouring_in = True
+    pour_in_products.update()
+    if pouring_in:
         time -= 1
     if time <= 0:
-        peppers.remove(pepper)
-        pepper = Pepper(pygame.transform.scale(load_image("pepper.png", -1), (66, 120)), 1, 1, 350, 350)
-        peppering = False
-        time = 50
-    peppers.draw(screen)
+        pour_in_products.remove(pepper)
+        pepper = PourInProduct(pygame.transform.scale(load_image("pepper.png", -1), (66, 120)), 1, 1, 350, 350)
+        pouring_in = False
+        time = 30
+    pour_in_products.draw(screen)
     pygame.display.flip()
     clock.tick(10)
 pygame.quit()

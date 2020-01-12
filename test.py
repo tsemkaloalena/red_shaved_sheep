b7dc88f3a815068e15ad6b21ae2e03cc73b8017f
tsemkaloalena <alenatsemkalo@gmail.com>
2020-01-12 21:48:27 +0300

    Добавила масло

--- test.py
+++ test.py
@@ -388,7 +388,7 @@ class PourInProduct(pygame.sprite.Sprite):
 
 class ProductToGrate(pygame.sprite.Sprite):
     def __init__(self, grated_product, product_size_x, product_size_y, product_x, product_y, grater_size,
-                 grater_x, grater_y, number_of_swipes, result, result_size, result_xy,  *group):
+                 grater_x, grater_y, number_of_swipes, result, result_size, result_xy, *group):
         super().__init__(group)
         self.done = False
 
@@ -444,6 +444,137 @@ class ProductToGrate(pygame.sprite.Sprite):
                     self.end_of_game()
                 else:
                     self.sprite.image = pygame.transform.scale(self.image, (self.ch_w, self.ch_h))
+
+
+class ProductToOil(pygame.sprite.Sprite):
+    def __init__(self, product, oiled_product, product_size_x, product_size_y, product_x, product_y, bowl_size, bowl_x,
+                 bowl_y, *group):
+        super().__init__(group)
+        self.done = False
+        self.oiling = False
+        self.bowls = pygame.sprite.Group()
+        self.brush = pygame.sprite.Group()
+        self.bowl_size = bowl_size, bowl_size
+        self.bowl_image = pygame.transform.scale(load_image('oil_with_brush.png', -1), self.bowl_size)
+        self.bowl = pygame.sprite.Sprite(self.bowls)
+        self.bowl.image = self.bowl_image
+        self.bowl.rect = self.bowl.image.get_rect()
+        self.bowl.rect.x = bowl_x
+        self.bowl.rect.y = bowl_y
+
+        self.cursor_image = pygame.transform.scale(load_image('brush.png', -1), (20, 100))
+        self.cursor = pygame.sprite.Sprite(self.brush)
+        self.cursor.image = self.cursor_image
+        self.cursor.rect = self.cursor.image.get_rect()
+
+        self.drawing = True
+        self.start_to_oil = False
+        self.x, self.y = product_x, product_y
+        self.img_size = product_size_x, product_size_y
+        self.product = pygame.sprite.Group()  # сам предмет
+        self.already_oil = pygame.sprite.Group()  # линии, которые остаются, чтобы было видно, где мы уже намазали масло
+
+        self.image = load_image(product, -1)
+        self.image = pygame.transform.scale(self.image, self.img_size)
+        self.sprite = pygame.sprite.Sprite()
+        self.sprite.image = self.image
+        self.sprite.mask = pygame.mask.from_surface(self.sprite.image)
+        self.sprite.rect = self.sprite.image.get_rect()
+        self.product.add(self.sprite)
+        self.sprite.rect.x = product_x
+        self.sprite.rect.y = product_y
+
+        self.board = load_image("board.png", -1)
+        self.board = pygame.transform.scale(self.board, self.img_size)
+        self.oiled = pygame.sprite.Sprite()
+        self.oiled.image = self.board
+        self.oiled.mask = pygame.mask.from_surface(self.oiled.image)
+        self.oiled.rect = self.oiled.image.get_rect()
+        self.oiled.rect.x = self.x
+        self.oiled.rect.y = self.y
+        self.already_oil.add(self.oiled)
+
+        self.oiled_product = load_image(oiled_product, -1)
+        self.oiled_product = pygame.transform.scale(self.oiled_product, self.img_size)
+
+    def check_oil(self):
+        if pygame.sprite.collide_mask(self.sprite, self.oiled):
+            m1 = self.sprite.mask
+            m2 = self.oiled.mask
+            m = m1.overlap_mask(m2, (0, 0))
+            if m.count() > (m1.count() - m.count()) // 2:
+                self.product.remove(self.sprite)
+                self.sprite = pygame.sprite.Sprite()
+                self.sprite.image = self.oiled_product
+                self.sprite.mask = pygame.mask.from_surface(self.sprite.image)
+                self.sprite.rect = self.sprite.image.get_rect()
+                self.sprite.rect.x = self.x
+                self.sprite.rect.y = self.y
+                self.product.add(self.sprite)
+                self.drawing = False
+                self.done = True
+                return True
+
+    def change_cursor(self, event):
+        if self.oiling:
+            pygame.mouse.set_visible(False)
+            self.cursor.rect.bottomleft = event.pos
+        else:
+            pygame.mouse.set_visible(True)
+
+    def change_bowl(self):
+        if self.oiling:
+            x = self.bowl.rect.x
+            y = self.bowl.rect.y
+            self.bowls.remove(self.bowl)
+            self.bowl_image = pygame.transform.scale(load_image('oil.png', -1), self.bowl_size)
+            self.bowl = pygame.sprite.Sprite(self.bowls)
+            self.bowl.image = self.bowl_image
+            self.bowl.rect = self.bowl.image.get_rect()
+            self.bowl.rect.x = x
+            self.bowl.rect.y = y
+        else:
+            x = self.bowl.rect.x
+            y = self.bowl.rect.y
+            self.bowls.remove(self.bowl)
+            self.bowl_image = pygame.transform.scale(load_image('oil_with_brush.png', -1), self.bowl_size)
+            self.bowl = pygame.sprite.Sprite(self.bowls)
+            self.bowl.image = self.bowl_image
+            self.bowl.rect = self.bowl.image.get_rect()
+            self.bowl.rect.x = x
+            self.bowl.rect.y = y
+
+    def draw_on_screen(self):
+        self.product.draw(screen)
+        if self.drawing:
+            self.already_oil.draw(screen)
+        self.bowls.update()
+        self.bowls.draw(screen)
+        if pygame.mouse.get_focused() and self.oiling:
+            self.brush.draw(screen)
+        self.brush.update()
+
+    def check_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.oiling:
+                self.start_to_oil = True
+        if event.type == pygame.MOUSEBUTTONUP:
+            self.start_to_oil = False
+            self.oiled.image = self.board
+            self.oiled.mask = pygame.mask.from_surface(self.oiled.image)
+            self.check_oil()
+        if self.start_to_oil and event.type == pygame.MOUSEMOTION:
+            pygame.draw.circle(self.board, (255, 255, 0), (event.pos[0] - self.x, event.pos[1] - self.y), 15)
+            self.oiled.image = self.board
+        if event.type == pygame.MOUSEMOTION:
+            self.change_cursor(event)
+        if event.type == pygame.MOUSEBUTTONDOWN and self.bowl.rect.collidepoint(event.pos):
+            if self.oiling:
+                self.oiling = False
+                self.change_bowl()
+            else:
+                self.oiling = True
+                self.change_bowl()
 
 
 running = True
@@ -862,7 +993,9 @@ def grate_stage(things_to_place):
 
     time = 300
 
-    product_to_grate = ProductToGrate(st[0] + '.png', int(st[1]), int(st[2]), int(st[3]), int(st[4]), (int(st[5]), int(st[6])), int(st[7]), int(st[8]), int(st[9]), st[10] + '.png', (int(st[11]), int(st[12])), (int(st[13]), int(st[14])))
+    product_to_grate = ProductToGrate(st[0] + '.png', int(st[1]), int(st[2]), int(st[3]), int(st[4]),
+                                      (int(st[5]), int(st[6])), int(st[7]), int(st[8]), int(st[9]), st[10] + '.png',
+                                      (int(st[11]), int(st[12])), (int(st[13]), int(st[14])))
 
     maxscore += time
     fon = pygame.transform.scale(load_image('table.jpg'), [500, 500])
@@ -871,9 +1004,6 @@ def grate_stage(things_to_place):
     time_rect = timetext.get_rect()
     time_rect.x = 470
     time_rect.y = 51
-
-    pour_times = 5
-    check_done = 0
 
     grate_moving = False
 
@@ -920,6 +1050,78 @@ def grate_stage(things_to_place):
             time -= 0.1
         if time <= 0:
             grate_running = False
+            start_running = True
+            game_running = False
+            lose()
+            return False
+        pygame.display.flip()
+        clock.tick(50)
+
+
+def oil_stage(things_to_place):
+    global running, maxscore, score, game_running, start_running
+    oil_running = True
+    font = pygame.font.SysFont('verdana', 20)
+    string_rendered = font.render('', 1, (255, 255, 255))
+    intro_rect = string_rendered.get_rect()
+    intro_rect.x = 10
+    intro_rect.y = 200
+
+    st = things_to_place[0].split()
+    recepy = font.render('Намаж маслом курицу', 1, (255, 0, 0))
+    recepy_rect = recepy.get_rect()
+    recepy_rect.x = 10
+    recepy_rect.y = 5
+
+    time = 300
+
+    # product_to_oil = ProductToGrate(st[0] + '.png', int(st[1]))
+    oil = ProductToOil("raw_chicken.png", "oiled_raw_chicken.png", 200, 200, 50, 100, 100, 300, 300)
+
+    maxscore += time
+    fon = pygame.transform.scale(load_image('table.jpg'), [500, 500])
+
+    timetext = font.render(str(time), 1, (255, 255, 255))
+    time_rect = timetext.get_rect()
+    time_rect.x = 470
+    time_rect.y = 51
+
+    oil_moving = False
+
+    while oil_running:
+        screen.blit(fon, (0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                oil_running = False
+                running = False
+                return False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if intro_rect.collidepoint(event.pos):
+                    return True
+            oil.check_event(event)
+        oil.draw_on_screen()
+
+        timetext = font.render(str(time).split('.')[0], 1, (255, 255, 255))
+        time_rect = timetext.get_rect()
+        time_rect.x = 450
+        time_rect.y = 5
+        screen.blit(string_rendered, intro_rect)
+        screen.blit(timetext, time_rect)
+        screen.blit(recepy, recepy_rect)
+
+        all_sprites.update()
+        if oil.done:
+            create_particles((250, 0))
+            score += time
+            font = pygame.font.SysFont('verdana', 20)
+            string_rendered = font.render('Перейти к следующему шагу', 1, (255, 255, 255))
+            intro_rect = string_rendered.get_rect()
+            intro_rect.x = 5
+            intro_rect.y = 470
+        if not oil.done:
+            time -= 0.1
+        if time <= 0:
+            oil_running = False
             start_running = True
             game_running = False
             lose()
@@ -1205,6 +1407,9 @@ while running:
                     break
             if stage[i] == 'grate':
                 if not grate_stage(things_to_place[i]):
+                    break
+            if stage[i] == 'oil':
+                if not oil_stage(things_to_place[i]):
                     break
             if stage[i] == 'oven':
                 if not oven_stage(things_to_place[i]):
